dfd92d50662abd2b4da693f92b53cc8eae2a4ddb
Bikrant <bikrantthakur6@gmail.com>
2021-12-07 22:57:40 +0530

    deletion from neginning and end is added

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -10,11 +10,22 @@ struct node{
 void beginsert(int);
 void display();
 void lastinsert(int);
+void delbeg();
+void delend();
 
 int main(){
     beginsert(10);
     beginsert(20);
+    beginsert(50);
+    beginsert(80);
+    beginsert(40);
     lastinsert(30);
+    display();
+    delbeg();
+    display();
+    delend();
+    display();
+    delend();
     display();
   
     return 0;
@@ -33,6 +44,26 @@ void beginsert(int value){
     }
 }
 
+void delbeg(){
+    if(head==NULL){
+        cout<<"\nUNDERFLOW!!";
+    }else{
+        temp=head;
+        head=head->next;
+        delete temp;
+    }
+}
+
+void delend(){
+    node *prev=new node();
+    temp=head;
+    while(temp->next!=NULL){
+        prev=temp;
+        temp=temp->next;
+    }
+    prev->next=NULL;
+    delete temp;
+}
 
 void lastinsert(int value){
     struct node *ptr=new node();
